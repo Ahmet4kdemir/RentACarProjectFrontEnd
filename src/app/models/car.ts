25540bbf72e2b6a4d90668e873c79146aff2579d
Ahmet Akdemir <ahmet.akdemir@st.uskudar.edu.tr>
2023-05-09 11:12:21 +0300

    first commit

---
+++ src/app/models/car.ts
@@ -0,0 +1,10 @@
+export interface Car{
+    carId:number;
+    brandId:number;
+    colorId:number;
+    carName:string;
+    modellYear:number;
+    dailyPrice:number;
+    description:string;
+
+}
