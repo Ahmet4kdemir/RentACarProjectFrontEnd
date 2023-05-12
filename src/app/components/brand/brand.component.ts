ead8f71c8f1177de0b24ba440684fd43c1163a8a
Ahmet Akdemir <ahmet.akdemir@st.uskudar.edu.tr>
2023-05-12 17:10:40 +0300

    car,color,brand tables

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Brand } from 'src/app/models/brand';
+import { BrandService } from 'src/app/services/brand.service';
 
 @Component({
   selector: 'app-brand',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./brand.component.css']
 })
 export class BrandComponent {
+  brands: Brand[] = [];
+  dataLoaded=false;
+  
 
+  constructor(private brandService:BrandService) {}
+
+  ngOnInit(): void {
+    this.getBrand();
+  }
+
+  getBrand() {
+    this.brandService.getBrand().subscribe(response=>{
+      this.brands=response.data
+      this.dataLoaded=true;
+    })
+  }
 }
