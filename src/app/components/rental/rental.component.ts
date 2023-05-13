05ac0f7dac5dcbd1ccf96337b3b1f157eff54722
Ahmet Akdemir <ahmet.akdemir@st.uskudar.edu.tr>
2023-05-13 16:44:35 +0300

    rentaldetails added.

---
+++ src/app/components/rental/rental.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Rental } from 'src/app/models/rental';
+import { RentalService } from 'src/app/services/rental.service';
+
+@Component({
+  selector: 'app-rental',
+  templateUrl: './rental.component.html',
+  styleUrls: ['./rental.component.css']
+})
+export class RentalComponent {
+  rentals: Rental[] = [];
+  dataLoaded=false;
+  
+
+  constructor(private rentalService:RentalService) {}
+
+  ngOnInit(): void {
+    this.getRentals();
+  }
+
+  getRentals() {
+    this.rentalService.getColors().subscribe(response=>{
+      this.rentals=response.data
+      this.dataLoaded=true;
+    })
+  }
+}
